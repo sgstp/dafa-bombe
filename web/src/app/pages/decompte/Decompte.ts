56c81ac99ca122a1df895826eb824b05d5d6ca2b
Samuel Gauvin-St-Pierre <samuel.gauvin-st-pierre@dti.ulaval.ca>
2019-05-08 14:46:27 -0400

    Ajout du code [2,8,9,7,5,0,3,4,1,6]

--- web/src/app/pages/decompte/Decompte.ts
+++ web/src/app/pages/decompte/Decompte.ts
@@ -8,6 +8,7 @@ import Lodash from "lodash";
 
 let INTERVAL_MAJ_DONNEES: number = 400;
 let INTERVAL_MAJ_BUZZER: number = 900;
+let CODE: number[] = [2,8,9,7,5,0,3,4,1,6];
 
 @WithRender
 @Component({})
@@ -18,6 +19,7 @@ export default class PageDecompte extends Vue {
 
     buzzer: HTMLAudioElement;
     intervalBuzzer: Timeout;
+    code: number[] = [0,1,2,3,4,5,6,7,8,9];
 
     created(): void {
         this.intervalMaj = setInterval(this.majDonnees, INTERVAL_MAJ_DONNEES);
@@ -32,6 +34,7 @@ export default class PageDecompte extends Vue {
     mounted(): void {
         this.buzzer = this.$refs.buzzer as HTMLAudioElement;
         this.intervalBuzzer = setInterval(this.jouerBuzzer, INTERVAL_MAJ_BUZZER);
+        console.log('Chargement buzzer');
     }
 
     majDonnees(): void {
@@ -42,7 +45,7 @@ export default class PageDecompte extends Vue {
 
     jouerBuzzer(): void {
         if (this.etat.buzzerActif) {
-            this.buzzer.play().then();
+            this.buzzer.play();
         } else {
             this.buzzer.pause();
         }
@@ -57,9 +60,39 @@ export default class PageDecompte extends Vue {
     }
 
     get afficherDecompte(): boolean {
-        return !this.etat.desarmer;
+        return !this.etat.desarmer && !this.afficherArme;
     }
     get afficherDesarme(): boolean {
         return this.etat.desarmer;
+    }
+    get afficherArme(): boolean {
+        return !this.etat.decompteEnCours && !this.afficherDesarme;
+    }
+    get afficherCode(): boolean {
+        return this.etat.afficherCode;
+    }
+
+    ajouterCode(val: number): void {
+        this.code = Lodash.pull(this.code, val);
+        this.code.unshift(val)
+
+        let estCodeEgual = (): boolean => {
+            let ok = true
+            this.code.forEach((v,i) => {
+                console.log(CODE[i]);
+                if (v != CODE[i]) {
+                    ok = false;
+                }
+            });
+            return ok;
+        }
+
+        if (estCodeEgual()) {
+            clearInterval(this.intervalMaj);
+            this.etat.desarmer = true;
+            this.etat.afficherCode = false;
+            this.etat.buzzerActif = false;
+            this.etat.decompteEnCours = false;
+        }
     }
 }
--- web/src/app/utils/Config.ts
+++ web/src/app/utils/Config.ts
@@ -6,8 +6,8 @@ export default class Config {
     static intervalMajApi: number = 1000;
     // @ts-ignore
     // static urlApi: string = '/';
-    // static urlApi: string = 'http://localhost/';
-    static urlApi: string = 'http://192.168.0.170/';
+    static urlApi: string = 'http://localhost/';
+    // static urlApi: string = 'http://192.168.0.170/';
 
     static pageDecompte = 'pageDecompte';
 }
